3498bcc1092c8f5607f3974829a42eafc077c764
drspac <drspac@163.com>
2016-03-24 15:25:57 +0800

    practice 4.5

--- hello.py
+++ hello.py
@@ -5,11 +5,20 @@
 from flask import Flask
 from flask import render_template
 from flask import url_for
+from flask import session
 from flask import redirect
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
+
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+	name = StringField('What is you name?', validators=[Required()])
+	submit = SubmitField('Submit')
 
 
 app = Flask(__name__)
@@ -18,13 +27,19 @@ manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+app.config['SECRET_KEY'] = 'This key is hard to guess'
+
 @app.route('/user/<name>')
 def user(name):
 	return render_template("user.html", name=name)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html', current_time=datetime.utcnow())	
+	form = NameForm()
+	if form.validate_on_submit():
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html', form=form, name=session.get('name'))	
 @app.route('/base')
 def base():
 	return render_template('base.html')
