8b4916c1da2ed35eb68f2457e0b9208c4d088407
drspac <drspac@163.com>
2016-03-25 14:35:58 +0800

    mail practice 6.4 Not finished

--- hello.py
+++ hello.py
@@ -14,6 +14,9 @@ from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.mail import Mail
+from flask.ext.mail import Message
+
 from datetime import datetime
 
 from flask.ext.wtf import Form
@@ -22,17 +25,26 @@ from wtforms.validators import Required
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
-app.config['SECRET_KEY'] = 'This key is hard to guess'
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-
 app = Flask(__name__)
 app.debug = True
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+mail = Mail(app)
+
+app.config['SECRET_KEY'] = 'This key is hard to guess'
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+app.config['MAIL_SERVER'] = 'smtp.163.com'
+app.config['MAIL_PORT'] = 25
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flask]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 
 class NameForm(Form):
@@ -47,14 +59,16 @@ class Role(db.Model):
 
 	def __repr__(self):
 		return '<Role %r>' % self.name
+	users = db.relationship('User', backref='role')
 
 class User(db.Model):
-	__tablename__ == 'users'
+	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), unique=True, index=True) 
 
 	def __repr__(self):
 		return '<User %r>' % self.name
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 @app.route('/user/<name>')
 def user(name):
@@ -67,6 +81,7 @@ def index():
 		old_name = session.get('name')
 		if old_name is not None and old_name != form.name.data:
 			flash('Looks like you have changed your name!')
+			send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user')
 		session['name'] = form.name.data
 		return redirect(url_for('index'))
 	return render_template('index.html', form=form, name=session.get('name'))	
@@ -84,6 +99,14 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html'), 500
+
+
+def send_email(to, subject, template, **kwargs):
+	msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+	msg.body = render_template(template + '.txt', **kwargs)
+	msg.html = render_template(template + '.html', **kwargs)
+	mail.send(msg)
+
 
 if __name__ == '__main__':
 	manager.run()
