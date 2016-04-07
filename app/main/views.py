c4f67108a2af16fca1c881df310fda74291fc949
drspac <drspac@iZ23le0n2tuZ.(none)>
2016-04-07 17:11:37 +0800

    added pages navigation. bug: can't post well

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, current_app, flash
+from flask import render_template, session, redirect, url_for, current_app, flash, request
 from ..email import send_email
 from . import main
 from .forms import NameForm, EditProfileForm, EditProfileAdminForm, PostForm
@@ -10,23 +10,30 @@ from ..decorators import admin_required
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-	form = NameForm()
 	form = PostForm()
+	print "current_user.can: %s" % current_user.can(Permission.WRITE_ARTICLES)
 	if current_user.can(Permission.WRITE_ARTICLES) and form.validate_on_submit():
+	#if form.validate_on_submit():
 		post = Post(body = form.body.data, \
 			author = current_user._get_current_object())
 		db.session.add(post)
 		return redirect(url_for('.index'))
 	posts = Post.query.order_by(Post.timestamp.desc()).all()
+	page = request.args.get('page', 1, type = int)
+	pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+			page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+			error_out = False)
+	posts = pagination.items
 	return render_template('index.html', 
-			form = form, posts = posts)
+			form = form, posts = posts, pagination = pagination)
 
 @main.route('/user/<username>')
 def user(username):
-	user = User.query.filter_by(username=username).first()
+	user = User.query.filter_by(username = username).first()
 	if user is None:
 		abort(404)
-	return render_template('user.html', user=user)
+	posts = user.posts.order_by(Post.timestamp.desc()).all()
+	return render_template('user.html', user = user, posts = posts)
 
 @main.route('/edit-profile', methods = ['GET', 'POST'])
 @login_required
