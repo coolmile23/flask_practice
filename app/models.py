141d7e412b3490156fb600dec20578fea0722e8a
coolmile23 <coolmile23@gmail.com>
2016-04-11 17:31:57 +0800

    show follower and following, still have some bugs

--- app/models.py
+++ app/models.py
@@ -35,6 +35,7 @@ class Role(db.Model):
 			'User': (Permission.FOLLOW |
 				Permission.COMMENT |
 				Permission.WRITE_ARTICLES, True),
+
 			'Moderator': (Permission.FOLLOW |
 					Permission.COMMENT |
 					Permission.WRITE_ARTICLES |
@@ -51,6 +52,13 @@ class Role(db.Model):
 		db.session.commit()
 	def __repr__(self):
 		return '<Role %r>' % self.name
+class Follow(db.Model):
+	__tablename__ = 'follows'
+	follower_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+				primary_key = True)
+	followed_id = db.Column(db.Integer, db.ForeignKey('users.id'), 
+				primary_key = True)
+	timestamp = db.Column(db.DateTime, default = datetime.utcnow)	
 
 class User(UserMixin, db.Model):
 	__tablename__ = 'users'
@@ -66,6 +74,18 @@ class User(UserMixin, db.Model):
 	password_hash = db.Column(db.String(128))
 	confirmed = db.Column(db.Boolean, default = False)
 	avatar_hash = db.Column(db.String(32))
+
+	followed = db.relationship('Follow',
+				foreign_keys = [Follow.follower_id],
+				backref = db.backref('follower', lazy = 'joined'),
+				lazy = 'dynamic',
+				cascade = 'all, delete-orphan')
+
+	followers = db.relationship('Follow',
+				foreign_keys = [Follow.followed_id],
+				backref = db.backref('followed', lazy = 'joined'),
+				lazy = 'dynamic',
+				cascade = 'all, delete-orphan')
 
 	posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
 
@@ -86,7 +106,8 @@ class User(UserMixin, db.Model):
 		return True
 
 	def can(self, permissions):
-		print 'permisions = %s' % permissions
+		print 'permisions = %s' % hex(permissions)
+		print 'role: %s' % self.role
 		return self.role is not None and \
 			(self.role.permissions & permissions) == permissions
 
@@ -157,6 +178,28 @@ class User(UserMixin, db.Model):
 				db.session.commit()
 			except IntegrityError:
 				db.session.rollback()	
+	
+	def follow(self, user):
+		if not self.is_following(user):
+			f = Follow(follower = self, followed = user)
+			db.session.add(f)
+
+	def unfollow(self, user):
+		f = self.followed.filter_by(followed_id = user.id).first()
+		if f:
+			db.session.delete(f)
+
+	def is_following(self, user):
+		return self.followed.filter_by(followed_id = user.id).first() is not None
+
+	def is_followed_by(self, user):
+		return self.follower.filter_by(
+			follower_id = user.id).first() is not None
+
+
+
+
+
 class AnonymousUser(AnonymousUserMixin):
 	def can(self, permission):
 		return False
@@ -196,6 +239,8 @@ class Post(db.Model):
 								tags = allowed_tags, strip = True))
 
 		db.envent.listen(Post.body, 'set', Post.on_changed_body)
+
+
 
 login_manager.anonymous_user = AnonymousUser
 
